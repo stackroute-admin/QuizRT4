989c951e83867a7c490c03d37a41e218f32d0bec
abskr82 <abhishekkumar647@outlook.com>
2016-05-03 18:30:18 +0530

    modifying return value of counter functions

--- routes/badgesManager/counterEvaluater/counterEvaluator.js
+++ routes/badgesManager/counterEvaluater/counterEvaluator.js
@@ -2,28 +2,28 @@ var getGameStat = require('../../getGameStat');
 var Q = require('q');
 
 module.exports = {
-    // get total win count for a user
+    // 1:> get total win count for a user
     getNumOfWin :function(userId,done){
         getGameStat.getWinCountForUser([userId])
         .then(function(data) {
-            done(null,data);
+            done(null,data[0].wins);
         })
         .fail(function(err) {
             done(err,null);
         });
     },
-    // get number of consecutive win for a user
+    // 2:> get number of consecutive win for a user
     getNumOfConsWin :function(userId,done){
         getGameStat.getUserNOfConsWin(userId)
         .then(function(data) {
-            done(null,data);
+            done(null,data.winCount);
         })
         .fail(function(err) {
             done(err,null);
         });
     },
 
-    // get average response time for correct answer
+    // 3:> get average response time for correct answer
     avgResTimeCrct :function(userId,done){
         getGameStat.getUserAvgRespTime(userId)
         .then(function(data) {
@@ -34,7 +34,7 @@ module.exports = {
         });
     },
 
-    // get distinct topic played count
+    // 4:> get distinct topic played count
     getNumOfUniqueTopicPlayed: function(userId, done) {
         getGameStat.getTopicPlayedForUser(userId)
         .then(function(data) {
@@ -44,7 +44,7 @@ module.exports = {
             done(err,null);
         });
     },
-    // get number of games played by a user
+    // 5:> get number of games played by a user
     getNumOfGamePlayed:function(userId,done) {
         getGameStat.getUserGamePlayedCount(userId)
         .then(function(data) {
@@ -54,7 +54,7 @@ module.exports = {
             done(err,null);
         });
     },
-    // get monthly visit count for current month
+    // 6:> get monthly visit count for current month
     getUserLoginCount: function(userId,done) {
         getGameStat.getMonthlyVisitCount(userId)
         .then(function(data) {
