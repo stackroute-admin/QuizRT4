960581ec9ea8a50e83aef60362b9967f0ae7e30c
kontactmainak <kontactmainak@gmail.com>
2016-05-04 10:30:50 +0530

    added code for notification view

--- public/js/controllers/notificationController.js
+++ public/js/controllers/notificationController.js
@@ -1,7 +1,25 @@
-angular.module('quizRT').controller('notificationController', function($scope, $rootScope, $routeParams) {
-  // $scope.checkNotifications = function() {
-  //   $rootScope.notificationSocket.emit('clientNotification', 'testuser');
-  //
-  // };
+angular.module('quizRT')
+  .controller('notificationController', function($scope, $routeParams, $http, $rootScope, $location) {
+    $http.get('/notifications')
+      .success(function(data, status, headers, config) {
+        $scope.notificationData = data;
+      });
 
-});
+      $scope.sendNotification = function(userChoice){
+        if (userChoice==='accept'){
+            //call updater to mark seen false
+            //pass the meta object to the calling guy .
+            //depends on req type
+
+
+        }
+        else if (userChoice==='reject') {
+          //call updater to mark seen false
+          //pass the meta object to the calling guy .
+          //depends on req type
+        }
+        else{
+          //keep quiet
+        }
+      }
+  });
--- public/js/notificationDirective.js
+++ public/js/notificationDirective.js
@@ -1,44 +1,41 @@
 'use strict';
 //  scope:{notificationCount:"="},
 angular.module('quizRT')
-  .directive('notificationData', ['$rootScope', '$http','$location', function($rootScope, $http,$location) {
+  .directive('notificationData', ['$rootScope', '$http', '$location', function($rootScope, $http, $location) {
     return {
       restrict: 'E',
       template: '<a ng-click="showModal()">{{notificationCount}}</a>',
       link: function postLink(scope, element, attrs) {
 
         $rootScope.$on('sent:a:frndreq', function(event, data) {
-        scope.notificationCount+=1;
+          scope.notificationCount += 1;
         });
         $http.get('/notifications')
           .success(function(data, status, headers, config) {
             scope.notificationCount = data.length;
             scope.$root.notificationCount = data.length;
             scope.notificationData = data;
-            console.log(data);
-
           });
         scope.showModal = function() {
-            //show the modal
-            $rootScope.notificationSocket.emit('respond:to:frndreq', scope.notificationData);
+          //show the modal
+          $rootScope.notificationSocket.emit('respond:to:frndreq', scope.notificationData);
+        }
 
-          }
-
-          element.bind('click', function() {
-              $location.path('/notifications');
-          });
-          // element.bind('click', function() {
-          //
-          //   console.log(scope.notificationData);
-          // });
-          // attrs.$observe('notificationData',function(newVal){
-          //   var newSign = parseFloat(newVal);
-          //   if(newSign>0){
-          //     element[0].style.color='Green';
-          //   }else{
-          //       element[0].style.color='Red';
-          //   }
-          // });
+        element.bind('click', function() {
+          $location.path('/notifications');
+        });
+        // element.bind('click', function() {
+        //
+        //   console.log(scope.notificationData);
+        // });
+        // attrs.$observe('notificationData',function(newVal){
+        //   var newSign = parseFloat(newVal);
+        //   if(newSign>0){
+        //     element[0].style.color='Green';
+        //   }else{
+        //       element[0].style.color='Red';
+        //   }
+        // });
       }
     };
   }]);
