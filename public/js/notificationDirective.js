f8c3cad9eb78900c5e594288732b7e1e4a920290
flipsyde606 <flipsyde606@gmail.com>
2016-05-10 17:48:01 +0530

    fixed notification click

--- public/js/notificationDirective.js
+++ public/js/notificationDirective.js
@@ -4,13 +4,13 @@ angular.module('quizRT')
   .directive('notificationData', ['$rootScope', '$http', '$location', function($rootScope, $http, $location) {
     return {
       restrict: 'E',
-      template: '<span class="btn notification-title glyphicon glyphicon-globe" ng-click="this.showNotifications()"><span class="notification-counter">{{notificationCount}}</span></span>',
+      template: '<span ng-click="postLink()" class="btn notification-title glyphicon glyphicon-globe"><span class="notification-counter">{{notificationCount}}</span></span>',
       link: function postLink(scope, element, attrs) {
-        var showNotifications = function() {
-          $location.path('/notifications');
-        }
         element.bind('click', function() {
-          $location.path('/notifications');
+          console.log('bind here');
+          scope.$apply(function () {
+             $location.path('/notifications');
+          });
         });
       }
     };
