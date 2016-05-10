a1d846431de029427d074a0615690b74d211e6a1
flipsyde606 <flipsyde606@gmail.com>
2016-05-10 17:16:17 +0530

    notification UI

--- public/js/notificationDirective.js
+++ public/js/notificationDirective.js
@@ -4,7 +4,7 @@ angular.module('quizRT')
   .directive('notificationData', ['$rootScope', '$http', '$location', function($rootScope, $http, $location) {
     return {
       restrict: 'E',
-      template: '<span class="btn notification-title" ng-click="this.showNotifications()">Notifications<span class="notification-counter">{{notificationCount}}</span></span>',
+      template: '<span class="btn notification-title glyphicon glyphicon-globe" ng-click="this.showNotifications()"><span class="notification-counter">{{notificationCount}}</span></span>',
       link: function postLink(scope, element, attrs) {
         var showNotifications = function() {
           $location.path('/notifications');
